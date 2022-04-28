d70c4df96e3ce19a36181ca238f52617c1630c29
foam60 <antoine.mousse@gmail.com>
2022-04-28 11:09:49 +0200

    push

--- api/index.js
+++ api/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require("mongoose")
+const { MongoClient } = require("mongodb");
 const app = express();
 const port = 3000;
+var url = 'mongodb://admin:admin@mongo:27017';
+
+
+
 
 app.get('/', (req, res) => {
   res.send('TEST APIIII')
+});
+
+app.get('/getDB', (req, res) => {
+    MongoClient.connect("mongodb://root:password@mongodb:27017")
+    .then(()=>{
+      console.log("db connect success");
+    })
+    .catch((err)=>{
+      throw err
+    });
+    res.send('TEST APIIII db connect success')
 });
 
 app.listen(port, () => {
